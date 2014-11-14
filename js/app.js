ba8ca8247c3a5770d0711e53754cac0a928e51dc
njarvis <npjarvis@uw.edu>
2014-11-13 23:12:07 -0800

    added controller, trying to figure out img src

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
-/* 
+/*
     app.js
     Angular application for the address book challenge
 
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+"use strict";
+
+angular.module('AddressBook', [])
+    .controller('AddressController', function($scope) {
+        $scope.employees = pawneeEmployees;
+        $scope.sortCol = 'lastName';
+
+    });
