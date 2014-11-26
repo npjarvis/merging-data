565d5d2da67247ff7c1f8f4b92419287845d45eb
njarvis <npjarvis@uw.edu>
2014-11-26 14:59:20 -0800

    finished

--- js/app.js
+++ js/app.js
@@ -7,10 +7,29 @@
 */
 "use strict";
 
-angular.module('AddressBook', [])
-    .controller('AddressController', function($scope) {
-        $scope.employees = pawneeEmployees;
-        $scope.sortCol = 'lastName';
-        $scope.phone = 'phone';
+var bookModule = angular.module('AddressBook', []);
 
-    });
+// creates controller, referenced in <body> tag
+bookModule.controller('AddressController', function($scope) {
+    //references employeees.js array
+    $scope.employees = pawneeEmployees;
+
+    // order by lastName initially
+    $scope.sortCol = 'lastName';
+
+    // sortBy function is necessary to enable buttons, passed sortCol,
+    // the variable that determines how the list is sorted
+    // sortReverse enables sort desc/asc
+    $scope.sortBy = function(sortCol) {
+        if ($scope.sortCol == sortCol) {
+            $scope.sortReverse = !$scope.sortReverse;
+        }
+        else {
+            $scope.sortCol = sortCol;
+            $scope.sortReverse = false;
+        }
+    };
+    $scope.isSortedBy = function(colName) {
+        return colName == $scope.sortCol;
+    };
+});
